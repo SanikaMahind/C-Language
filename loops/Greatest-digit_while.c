93c1e206be354c5500879522b2360975b9c6db3f
sanika mahind <mahindsanika03@gmail.com>
2025-12-24 22:29:47 +0530

    Create Greatest-digit_while.c

---
+++ loops/Greatest-digit_while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num, digit, max = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        digit = num % 10;
+        if (digit > max)
+            max = digit;
+        num = num / 10;
+    }
+
+    printf("Greatest digit = %d\n", max);
+
+    return 0;
+}
