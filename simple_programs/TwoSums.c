4af29655769c667dc959be7732a0e8f4875ab7ba
sanika mahind <mahindsanika03@gmail.com>
2026-02-08 21:47:23 +0530

    Create TwoSums.c

---
+++ simple_programs/TwoSums.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *result = (int *)malloc(2 * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    return result;
+}
+
+
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int returnSize;
+
+    int *result = twoSum(nums, 4, target, &returnSize);
+
+    if (returnSize == 2) {
+        printf("Indices: %d %d\n", result[0], result[1]);
+    } else {
+        printf("No solution found\n");
+    }
+
+    free(result);
+    return 0;
+}
