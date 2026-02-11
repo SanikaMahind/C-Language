1d1eebc3d649456a08e18eb0a2c1708d2b81e0bf
sanika mahind <mahindsanika03@gmail.com>
2026-02-11 23:23:57 +0530

    Create leetcode.c

---
+++ simple_programs/leetcode.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int maxProfit(int* prices, int pricesSize) {
+    if (pricesSize == 0) return 0;
+
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int size = sizeof(prices) / sizeof(prices[0]);
+
+    int result = maxProfit(prices, size);
+
+    printf("Maximum Profit: %d\n", result);
+
+    return 0;
+}
